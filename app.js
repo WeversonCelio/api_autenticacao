f16764e8bded56d56cb30c796d4391df20ec6bf5
WeversonCelio <weversoncelio25@gmail.com>
2023-05-22 00:36:40 +0000

    criacao de usuario

--- app.js
+++ app.js
@@ -7,11 +7,64 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//configure json response
+app.use(express.json())
+
+
+// models
+const User = require('./model/User')
+
 //open route - public route
 app.get('/', (req,res)=>{
     res.status(200).json({msg: 'Bem vindo a nossa API'})
 })
 
+// register user
+app.post('/auth/register', async(req,res)=>{
+    const {name, email, password, confirmpassowrd} = req.body
+
+     //validation
+     if(!name){
+        return res.status(422).json({msg: 'O nome é obrigatorio' })
+     } 
+     if(!email){
+        return res.status(422).json({msg: ' O email é obrigatorio' })
+     }
+     if(!password){
+        return res.status(422).json({msg: 'a senha é obrigatoria' })
+     }
+     if(password!==confirmpassowrd){
+        return res.status(422).json({msg: 'a senha está divergente' })
+     }
+
+     //check if user exists
+     const userExists = await User.findOne({email: email})
+     if(userExists){
+        return res.status(422).json({msg: 'esse usuario já existe. Por favor, use outro email'})
+     }
+
+     //create password
+     const salt = await bcrypt.genSalt(12)
+     const passwordHast = await bcrypt.hash(password, salt)
+
+     //create user
+     const user = new User({
+        name,
+        email,
+        password: passwordHast
+     })
+
+     try {
+        await user.save()
+        res.status(201).json({msg: 'Usuario criado com sucesso!'})
+        
+     } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'})
+     }
+
+
+})
 
 // credenciais
 const dBUser = process.env.DB_USER
@@ -32,4 +85,3 @@ console.log('conectou ao banco')
 
 
 
-//        `mongodb://${DB_USER}:${DB_PASS}@192.168.3.10:27017`,)
